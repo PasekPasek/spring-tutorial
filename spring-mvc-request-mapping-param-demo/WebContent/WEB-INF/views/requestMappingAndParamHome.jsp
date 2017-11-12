9bef382f97aa331f69c8892cb1a72bb6178615e1
Pawel <pasekpasek@gmail.com>
2017-11-12 13:18:58 +0100

    Lesson 107 - RequestMapping - Class level mapping

---
+++ spring-mvc-request-mapping-param-demo/WebContent/WEB-INF/views/requestMappingAndParamHome.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>@RequestMapping and @requestParam test bed</title>
+<style type="text/css">
+input[type=text] {
+	padding: 10px;
+	margin: 10px;
+	font-size: 20px;
+}
+input[type=submit]{
+	border: none;
+	background-color: #4CAF50;
+	color: white;
+	margin: 16px;
+	cursor: pointer;
+	font-size: 20px;
+}
+
+h2{
+	color: #08298A;
+	text-align: center;
+}
+</style>
+</head>
+<body>
+
+</body>
+</html>
--- spring-mvc-request-mapping-param-demo/src/main/java/com/timbuchalka/springdemo/controller/test/RequestMappingAndParamDemoController.java
+++ spring-mvc-request-mapping-param-demo/src/main/java/com/timbuchalka/springdemo/controller/test/RequestMappingAndParamDemoController.java
@@ -3,7 +3,9 @@ package com.timbuchalka.springdemo.controller.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping(value="/requestMappingAndParamDemo")
 @Controller
@@ -16,5 +18,11 @@ public class RequestMappingAndParamDemoController {
 		return "requestMappingAndParamHome";
 	}
 	
-	
+	//test1: Testing @requestParam without explicit attributes
+	@RequestMapping(value="/test1")
+	public String requestMappingAndParamTest1(@RequestParam("@orgName") String orgName, Model model){
+		model.addAttribute("orgname", orgName);
+		model.addAttribute("testserial", "test1");
+		return "requestMappingAndParamResults";
+	}
 }
